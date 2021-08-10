1a03c9d99fe68fbe71ae0e3266b9e4025f667449
ktj1997 <rlaxowns7916@gmail.com>
2021-08-10 17:10:47 +0900

    fix:cors

--- project/application/src/main/java/com/project/configs/WebConfig.java
+++ project/application/src/main/java/com/project/configs/WebConfig.java
@@ -3,6 +3,7 @@ package com.project.configs;
 import com.project.security.AuthArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -10,7 +11,14 @@ import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+    }
+
+    @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthArgumentResolver());
     }
+
+
 }
