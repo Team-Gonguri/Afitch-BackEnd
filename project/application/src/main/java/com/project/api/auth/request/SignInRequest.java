00f413d6d4827438b382cf22aae47f0e566906dd
ktj1997 <rlaxowns7916@gmail.com>
2021-08-10 14:52:18 +0900

    feat: 비밀번호 정규식

--- project/application/src/main/java/com/project/api/auth/request/SignInRequest.java
+++ project/application/src/main/java/com/project/api/auth/request/SignInRequest.java
@@ -4,8 +4,10 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.checkerframework.checker.regex.qual.Regex;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @AllArgsConstructor
@@ -16,6 +18,7 @@ public class SignInRequest {
     private String accountId;
 
     @NotEmpty
-    @ApiModelProperty("비밀번호")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\\\d)[!-~₩]{8,16}$", message = "영문,숫자,특수문자가 포함되어야 하며, 8~16자 이내여야합니다.")
+    @ApiModelProperty("비밀번호(8~16, 특수문자 영어 숫자 최소하나씩")
     private String password;
 }
