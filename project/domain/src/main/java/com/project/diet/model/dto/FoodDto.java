5a7d5257e11ce6074a7ef35cf576cb2c361c22cd
ktj1997 <rlaxowns7916@gmail.com>
2021-08-15 22:15:40 +0900

    fix: food 관련 정보 ingredients depth 삭제

---
+++ project/domain/src/main/java/com/project/diet/model/dto/FoodDto.java
@@ -0,0 +1,40 @@
+package com.project.diet.model.dto;
+
+import com.project.diet.model.entity.Food;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class FoodDto {
+
+    private Long id;
+
+    private String name;
+
+    private String foodCategory;
+
+    private int size;
+
+    private String unit;
+
+    private double protein;
+
+    private double fat;
+
+    private double carbohydrate;
+
+
+    private double calories = 0;
+
+    public FoodDto(Food food) {
+        this.id = food.getId();
+        this.name = food.getName();
+        this.unit = food.getUnit();
+        this.size = food.getSize();
+        this.foodCategory = food.getFoodCategory();
+        this.protein = food.getIngredients().getProtein();
+        this.fat = food.getIngredients().getFat();
+        this.carbohydrate = food.getIngredients().getCarbohydrate();
+    }
+}
