d17f149f76c2ffca85955fb538a1b8f0dd91146a
ktj1997 <rlaxowns7916@gmail.com>
2021-08-21 00:36:36 +0900

    feat: webm파일 지원, 동영상 파일 아니면 오류

--- core/src/main/java/com/project/exception/GlobalExceptionType.java
+++ core/src/main/java/com/project/exception/GlobalExceptionType.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum GlobalExceptionType {
 
     INVALID_ARGUMENT("SYSTEM-001", "잘못된 요청 파라미터 입니다."),
-    UNEXPECTED_SERVER_ERROR("SYSTEM-002", "예기치못한 서버 오류가 발생하였습니다.");
+    UNEXPECTED_SERVER_ERROR("SYSTEM-002", "예기치못한 서버 오류가 발생하였습니다."),
+    INVALID_FILE_TYPE("SYSTEM-003", "비디오 형식의 파일이아닙니다.");
 
 
     private final String code;
